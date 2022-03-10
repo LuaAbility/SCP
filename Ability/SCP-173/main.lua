08cd5b7df94ef5ce27367444cf3ed5e894e55936
MINUTE1084 <swpro1004@gmail.com>
2022-03-10 15:56:08 +0900

    조각상 능력 토글 추가

--- Ability/SCP-173/main.lua
+++ Ability/SCP-173/main.lua
@@ -4,21 +4,28 @@ function Init(abilityData)
 	plugin.registerEvent(abilityData, "SCP173-cancelMove1", "PlayerMoveEvent", 0)
 	plugin.registerEvent(abilityData, "SCP173-cancelMove2", "VehicleMoveEvent", 0)
 	plugin.registerEvent(abilityData, "SCP173-cancelOrKill", "EntityDamageEvent", 0)
+	plugin.registerEvent(abilityData, "SCP173-enable", "PlayerInteractEvent", 0)
 end
 
 function onEvent(funcTable)
-	if funcTable[1] == "SCP173-cancelMove1" then cancelMove1(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
-	if funcTable[1] == "SCP173-cancelMove2" then cancelMove2(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
-	if funcTable[1] == "SCP173-cancelOrKill" and funcTable[2]:getEventName() == "EntityDamageByEntityEvent" then cancelOrKill(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
+	if funcTable[1] == "SCP173-enable" then enable(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
+	if funcTable[3]:getVariable("SCP173-useAbility") then
+		if funcTable[1] == "SCP173-cancelMove1" then cancelMove1(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
+		if funcTable[1] == "SCP173-cancelMove2" then cancelMove2(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
+		if funcTable[1] == "SCP173-cancelOrKill" and funcTable[2]:getEventName() == "EntityDamageByEntityEvent" then cancelOrKill(funcTable[3], funcTable[2], funcTable[4], funcTable[1]) end
+	end
 end
 
 function onTimer(player, ability)
 	if player:getVariable("SCP173-canMove") == nil then 
 		player:setVariable("SCP173-canMove", true)
+		player:setVariable("SCP173-useAbility", true)
 	end
 	
-	seeCheck(player)
-	addEffect(player)
+	if player:getVariable("SCP173-useAbility") then
+		seeCheck(player)
+		addEffect(player)
+	end
 end
 
 function seeCheck(player)
@@ -75,4 +82,24 @@ function getLookingAt(player, player1)
 	if not player:hasLineOfSight(player1) then dot = 0 end
 	
 	return dot > 0.6
+end
+
+function enable(LAPlayer, event, ability, id)
+	if event:getAction():toString() == "LEFT_CLICK_AIR" or event:getAction():toString() == "LEFT_CLICK_BLOCK" then
+		if event:getItem() ~= nil then
+			if game.isAbilityItem(event:getItem(), "IRON_INGOT") then
+				if game.checkCooldown(LAPlayer, game.getPlayer(event:getPlayer()), ability, id) then
+					if game.getPlayer(event:getPlayer()):getVariable("SCP173-useAbility") then
+						game.getPlayer(event:getPlayer()):setVariable("SCP173-useAbility", false)
+						game.sendMessage(event:getPlayer(), "§2[§a조각상§2] §a능력을 비활성화했습니다.")
+						event:getPlayer():getWorld():spawnParticle(import("$.Particle").SMOKE_NORMAL, event:getPlayer():getLocation():add(0,1,0), 100, 0.5, 1, 0.5, 0.05)
+					else
+						game.getPlayer(event:getPlayer()):setVariable("SCP173-useAbility", true)
+						game.sendMessage(event:getPlayer(), "§2[§a조각상§2] §a능력을 활성화했습니다.")
+						event:getPlayer():getWorld():spawnParticle(import("$.Particle").SMOKE_NORMAL, event:getPlayer():getLocation():add(0,1,0), 100, 0.5, 1, 0.5, 0.05)
+					end
+				end
+			end
+		end
+	end
 end
